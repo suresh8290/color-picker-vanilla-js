25755ed9f2e7d82bf2865e7075c2f8aaa727bf2f
sureshsahu8290 <sureshsahu8290@gmail.com>
2026-01-03 22:19:29 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,9 @@
+const parent=document.getElementById("parent");
+
+parent.addEventListener('click',(e)=>{
+    const child=e.target;
+    const body=document.querySelector("body");
+    body.style.backgroundColor=child.id;
+
+})
+
